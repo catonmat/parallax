f7d9bd411cd939e5b28774b5730dedcbbaa3de0a
Richard Jarram <rjarram@me.com>
2019-06-27 20:35:04 -0400

    created scroll lightbox

--- helpers/scraper.rb
+++ helpers/scraper.rb
@@ -3,13 +3,14 @@ require 'nokogiri'
 
 gallery1_url = "https://anajovmir.com/projects/6053859"
 gallery1_images = Nokogiri::HTML(open(gallery1_url)).search('img').map { |img| img.attr('src') }
-p gallery1_images
+p gallery1_images.count
 
 gallery2_url = "https://anajovmir.com/projects/6053865"
 gallery2_images = Nokogiri::HTML(open(gallery2_url)).search('img').map { |img| img.attr('src') }
-p gallery2_images
+p gallery2_images.count
 
 gallery3_url = "https://anajovmir.com/projects/6053875"
 gallery3_images = Nokogiri::HTML(open(gallery3_url)).search('img').map { |img| img.attr('src') }
-p gallery3_images
+p gallery3_images.count
 
+p "Wrote all image urls to file."
